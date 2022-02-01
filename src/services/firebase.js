07b9763059beebfc8867649fdaedfb29c8644b3f
iagob28 <61669995+iagob28@users.noreply.github.com>
2022-02-01 15:58:26 -0300

    .env change

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: process.env.REACT_APP_VERCEL_API_KEY,
+  authDomain: process.env.REACT_APP_VERCEL_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_VERCEL_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_VERCEL_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_VERCEL_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_VERCEL_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
